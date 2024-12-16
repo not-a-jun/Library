60f7d37e0062601d4f51d6a69634e5dee53284a2
not-a-jun <ivannikolaev78286@gmail.com>
2024-12-16 17:46:52 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,78 +1,27 @@
-import java.util.ArrayList;
+import java.util.List;
 
-    class Book {
-        String title;
-        String author;
-        int year;
-        boolean isAvailable;
+public class Main {
+    public static void main(String[] args) {
 
-        public Book(String title, String author, int year) {
-            this.title = title;
-            this.author = author;
-            this.year = year;
-            this.isAvailable = true;
+        Library library = new Library();
+        Book book1 = new Book("Война и мир", "Лев Толстой", 1867, true);
+        Book book2 = new Book("Мы", "Евгений Замятин");
+        Book book3 = new Book("Анна Каренина", "Лев Толстой", 1873, true);
+
+        library.addBook(book1);
+        library.addBook(book2);
+        library.addBook(book3);
+
+        library.printAvailableBooks();
+
+        List<Book>tolstoyBooks = library.findBookByAuthor("Лев Толстой");
+        for (Book book: tolstoyBooks) {
+            book.displayInfo();
         }
 
-        public Book(String title, String author) {
-            this(title, author, 2024);
-        }
+        book1.borrowBook();
 
-        public void borrowBook() {
-            if (isAvailable) {
-                isAvailable = false;
-            }
-        }
+        library.printAvailableBooks();
 
-        public void returnBook() {
-            isAvailable = true;
-        }
-
-        public void displayInfo() {
-            System.out.println("Название книги:" + title + ", " + "Автор:" + author + ", " + "Год:" + year + ", " + "Доступность:" + isAvailable);
-        }
     }
-
-         class Library {
-            ArrayList<Book> books = new ArrayList<>();
-
-            public void addBook(Book book) {
-                books.add(book);
-            }
-
-            public void printAvailableBooks() {
-                for (Book book : books) {
-                    if (book.isAvailable) {
-                        book.displayInfo();
-                    }
-                }
-            }
-
-            public ArrayList<Book> findBookByAuthor(String author) {
-                ArrayList<Book> refund = new ArrayList<>();
-                for (Book book : books) {
-                    if (book.author.equals(author)) {
-                        refund.add(book);
-                    }
-                }
-                return refund;
-            }
-        }
-
-        public class Main {
-            public static void main(String[] args) {
-
-                Library library = new Library();
-                Book book1 = new Book("Война и мир", "Лев Толстой", 1867);
-                Book book2 = new Book("Мы", "Евгений Замятин", 1920);
-
-                library.addBook(book1);
-                library.addBook(book2);
-
-                library.printAvailableBooks();
-
-                book1.borrowBook();
-
-                library.printAvailableBooks();
-
-            }
-        }
+}
